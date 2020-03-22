d23e94235ddd0991e0c48712366ed623cd68e285
Strantonvad <nottna@gmail.com>
2020-03-22 10:43:32 +0300

    HomeWork #1

--- core/src/my/home/stargame/StarGame.java
+++ core/src/my/home/stargame/StarGame.java
@@ -13,12 +13,12 @@ public class StarGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("stormtrooper.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.72f, 0.5f, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
