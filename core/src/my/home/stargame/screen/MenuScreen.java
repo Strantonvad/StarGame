6bf8d3246dc6ab737338f52e67f24de90ae2b202
Strantonvad <nottna@gmail.com>
2020-03-24 16:17:40 +0300

    homework2

--- core/src/my/home/stargame/screen/MenuScreen.java
+++ core/src/my/home/stargame/screen/MenuScreen.java
@@ -11,16 +11,16 @@ public class MenuScreen extends BaseScreen {
 
     private Texture img;
     private Vector2 pos;
-    private Vector2 v;
-//    private float rotate;
+    private Vector2 dir;
+    private float goalX = 0;
+    private float goalY = 0;
+    private float SPEED = 3f;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
         pos = new Vector2();
-        v = new Vector2(0.3f, 0.3f);
-//        rotate = 0;
     }
 
     @Override
@@ -38,13 +38,32 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        pos.set(screenX, Gdx.graphics.getHeight() - screenY);
+        goalX = screenX;
+        goalY = Gdx.graphics.getHeight() - screenY;
         return false;
     }
 
     private void update(float delta) {
-        pos.add(v);
-//        rotate += 1;
+        dir = new Vector2(goalX - pos.x,goalY - pos.y).nor();
+
+        float posX = pos.x;
+        float posY = pos.y;
+        posX += dir.x * SPEED;
+        posY += dir.y * SPEED;
+
+        boolean isGoalPointReached = false;
+
+        if((dir.x > 0 && posX >= goalX) || (dir.x < 0 && posX <= goalX)) {
+            posX = goalX;
+            isGoalPointReached = true;
+        }
+        if((dir.y > 0 && posY >= goalY) || (dir.y < 0 && posY <= goalY)) {
+            posY = goalY;
+            isGoalPointReached = true;
+        }
+        if (!isGoalPointReached) {
+            pos.set(posX, posY);
+        }
     }
 
     private void draw() {
@@ -52,7 +71,6 @@ public class MenuScreen extends BaseScreen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         batch.draw(img, pos.x, pos.y);
-//        batch.draw(new TextureRegion(img), pos.x, pos.y, pos.x, pos.y, 250, 250, 1, 1, rotate);
         batch.end();
     }
 
